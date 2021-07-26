8631336bc99c89d20bfe62a7c0482d4697bc10de
_haks <1668938504@qq.com>
2021-07-26 19:11:50 +0800

    线性表-单链表:实现FindNode操作和Merge操作.

--- linear_list/link_list/link_list.cpp
+++ linear_list/link_list/link_list.cpp
@@ -24,35 +24,38 @@ Status DestroyList_L(LinkList &);
 Status InsertNode_L(LinkList &, int, ElemType); // 第二个参数是指定节点插入后其位序
 Status DeleteNode_L(LinkList &, int);           // 第二个参数是要删除结点的位序
 int LocateNode_L(LinkList, ElemType);           // 找出指定结点的位序
-Status FindNode_L(LinkList, int);               // 找出指定位序的结点
-Status Merge_L(LinkList &, LinkList);           // 要合并的两个链表都必须是非递减的，第二个链表合并到第一个链表
+Status FindNode_L(LinkList, int, ElemType &);   // 找出指定位序的结点
+Status Merge_L(LinkList &, LinkList &);         // 要合并的两个链表都必须是非递减的，第二个链表合并到第一个链表
 
+/* 主函数 */
 int main(void)
 {
     LinkList foo, bar;
-    CreateList_L(foo, 10);
-
-    DeleteNode_L(foo, 5);
-    InsertNode_L(foo, 5, 50);
-
-    printf("%d\n", LocateNode_L(foo, 100));
-    // FindNode_L(7);
-
-    int listLength = foo->data;
-    for (int i = 0; i < listLength; i++)
-    {
-        DeleteNode_L(foo, foo->data);
-    }
-
-    DestroyList_L(foo);
-
-    // printf("请确保接下来的两个链表是非递减的！\n");
+    ElemType elem;
     // CreateList_L(foo, 10);
-    // CreateList_L(bar, 5);
-    // Merge_L(foo, bar);
+
+    // DeleteNode_L(foo, 5);
+    // InsertNode_L(foo, 5, 50);
+
+    // printf("%d\n", LocateNode_L(foo, 9));
+    // FindNode_L(foo, 7, elem);
+    // printf("%d\n", elem);
+
+    // int listLength = foo->data;
+    // for (int i = 0; i < listLength; i++)
+    // {
+    //     DeleteNode_L(foo, foo->data);
+    // }
 
     // DestroyList_L(foo);
-    // DestroyList_L(bar);
+
+    printf("Note: Make sure the next two lists are non-decrement!\n");
+    CreateList_L(foo, 10);
+    CreateList_L(bar, 5);
+    Merge_L(foo, bar);
+
+    DestroyList_L(foo);
+    DestroyList_L(bar);
 
     return 0;
 }
@@ -100,15 +103,17 @@ Status CreateList_L(LinkList &list, int n)
 Status DestroyList_L(LinkList &list)
 {
     Node *p, *q;
+    int listLength;
     p = list;
+    listLength = list->data;
 
-    for (int i = 0; i < list->data; i++) // list->data是链表的长度
+    for (int i = 0; i < listLength; i++)
     {
         q = p->next;
         free(p);
         p = q;
     }
-    printf("INFO: Link list has been destroyed!");
+    printf("INFO: Link list has been destroyed!\n");
     return OK;
 }
 
@@ -195,4 +200,54 @@ int LocateNode_L(LinkList list, ElemType e)
     }
 
     return i;
+}
+
+Status FindNode_L(LinkList list, int i, ElemType &e)
+{
+    Node *p = list->next;
+    int j = 1;
+
+    while (p && j < i)
+    {
+        p = p->next;
+        j++;
+    }
+    if (!p || j > i)
+    {
+        return ERRO;
+    }
+
+    e = p->data;
+
+    return OK;
+}
+
+Status Merge_L(LinkList &la, LinkList &lb)
+{
+    Node *p, *q, *temp, *r;
+    p = la->next;
+    q = lb->next;
+    r = la;
+
+    while (p && q)
+    {
+        if (q->data < p->data)
+        {
+            temp = q->next;
+            q->next = r->next;
+            r->next = q;
+            q = temp;
+            r = r->next;
+        }
+        else
+        {
+            p = p->next;
+            r = r->next;
+        }
+    }
+    r->next = p ? p : q;
+    free(lb);
+    Display(la);
+
+    return OK;
 }
